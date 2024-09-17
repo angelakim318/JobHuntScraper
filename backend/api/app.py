ce27490313dbcbc655cfa22d0cc5238ad552ad92
Angela Kim <angelakim318@gmail.com>
2024-09-17 15:38:55 -0400

    updated path to models in app.py

--- backend/api/app.py
+++ backend/api/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from backend.models.models import SessionLocal, Job, User, ScrapeStatus, DATABASE_URL, init_db
+from models.models import SessionLocal, Job, User, ScrapeStatus, DATABASE_URL, init_db
 from sqlalchemy.exc import SQLAlchemyError
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
